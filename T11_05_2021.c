8cc943f460e510f6ffba57e9c0068309f0e571de
xteog <72068040+xteog@users.noreply.github.com>
2022-07-01 12:29:51 +0200

    Tema corretto

--- T11_05_2021.c
+++ T11_05_2021.c
@@ -19,47 +19,45 @@
 #define MAX_RIGHE 80
 
 
-struct biga
+typedef struct biga
 {
 	int tempo;
 	char stazione;
 	int bicicletta;
 	int azione;
-};
+} t_biga;
 
-struct biga* leggifile(char* nomefile, int* len)
+t_biga* leggifile(char* nomefile, int* len)
 {
-	struct biga *bike;
+	t_biga *bike, ciao;
 	int i;
-	int tempo;
-	char  stazione;
-	int bicicletta;
-	int azione;
 	char action[MAX_RIGHE + 1];
+
 	FILE *f;
-	f=fopen(nomefile,'r');
+	f=fopen(nomefile,"r");
 	if(f==NULL)
 		exit(1);
-	
+
+
 	for(i=0; feof(f)==0; i++)
 	{
-		fscanf(f,"%d %c %d %s", &bike<-tempo, &bike<-stazione, &bike<-bicicletta, action);
+		fscanf(f,"%d %c %d %s\n", &ciao.tempo, &ciao.stazione, &ciao.bicicletta, action);
 	}
 	
-	bike= malloc(sizeof(struct biga) * i);
-	
+	bike= malloc(sizeof(t_biga) * i);
+
 	if(bike==NULL)
-		{
+	{
 		printf("Errore di allocazione\n");
 		fclose(f);
 		exit(1);
-		}
+	}
 	
 	rewind(f);
 	
 	for(i=0; feof(f)==0; i++)
 	{
-		fscanf(f,"%d %c %d %s", &bike[i].tempo, &bike[i].stazione, &bike[i].bicicletta, action);
+		fscanf(f,"%d %c %d %s\n", &bike[i].tempo, &bike[i].stazione, &bike[i].bicicletta, action);
 		if(strcmp(action,"PICK")==0)
 			bike[i].azione=1;
 		else if (strcmp(action,"DROP")==0)
@@ -71,20 +69,20 @@ struct biga* leggifile(char* nomefile, int* len)
 }
 
 
-void complessivi(struct biga *bike, int len, int* cont1, int* cont2)
+void complessivi(t_biga *bike, int len, int* cont1, int* cont2)
 {
 	int i;
 	for(i=0; i<len; i++)
 	{
 		if(bike[i].azione==1)
-			*cont1++;
-			else
-			*cont2++;
+			(*cont1)++;
+		else
+			(*cont2)++;
 	}
 }
 
 
-void non_restituite(struct biga *bike, int len)
+void non_restituite(t_biga *bike, int len)
 {
 	int i,j,flag;
 	
@@ -101,13 +99,13 @@ void non_restituite(struct biga *bike, int len)
 			}
 			if(flag==1)
 			{
-			printf("%d\n",bike[i].bicicletta);
+				printf("%d\n",bike[i].bicicletta);
 			}
 		}
 	}
 }
 
-int stessa_stazione(struct biga *bike, int len)
+int stessa_stazione(t_biga *bike, int len)
 {
 	int i,j,flag,cont;
 	cont=0;
@@ -119,52 +117,58 @@ int stessa_stazione(struct biga *bike, int len)
 			for(j=i+1; j<len && flag==1;j++)
 			{
 				if(bike[i].stazione==bike[j].stazione && bike[i].bicicletta==bike[j].bicicletta && bike[j].azione==0)
-				cont++;
-				flag=0;
+				{
+					cont++;
+					flag=0;
+				}
 			}
 		}
 	}
 	return cont;
 }
 
-int media(struct biga *bike, int len)
+int media(t_biga *bike, int len)
 {
 	int i,j,somma,contatore; 
 	int flag;
 	float media;
+
 	contatore=0;
+	somma=0;
 	for(i=0; i<len;i++)
 	{
 		if(bike[i].azione==1)
 		{
 			flag=1;
-			somma=0;
-			 for(j=i+1; j<len && flag==1;j++)
-					{
-						if(bike[i].bicicletta==bike[j].bicicletta && bike[j].azione==0)
-							{
-								somma+=bike[j].tempo-bike[i].tempo;
-								contatore++;
-								flag=0;
-							}
-					}
+			for(j=i+1; j<len && flag==1; j++)
+			{
+				if(bike[i].bicicletta==bike[j].bicicletta && bike[j].azione==0)
+				{
+					somma+=(bike[j].tempo-bike[i].tempo);
+					contatore++;
+					flag=0;
+				}
+			}
 		}
 	}
+
 	if(contatore==0)
 		media=0.0;
-	else 
-	media=somma/contatore;
+	else
+		media=somma/contatore;
+
 	return media;
 }
 
-int main(int argc, int* argv[])
+int main(int argc, char* argv[])
 {
-	struct biga *bike;
+	t_biga *bike;
 	int len;
 	int cont1;
 	int cont2;
 	cont1=0;
 	cont2=0;
+
 	
 	bike=leggifile(argv[1], &len);
 	
@@ -175,13 +179,12 @@ int main(int argc, int* argv[])
 	
 	printf("[PERSE]\n");
 	non_restituite(bike,len);
-	printf("\n");
 	
 	printf("[STESSA_STAZIONE]\n");
-	printf("%d",stessa_stazione(bike,len));
+	printf("%d\n",stessa_stazione(bike,len));
 	
 	printf("[INTERVALLO]\n");
-	printf("%d\n",media(bike, len));
+	printf("%.2f\n",media(bike, len));
 	
 	free(bike);
 	return 0;
